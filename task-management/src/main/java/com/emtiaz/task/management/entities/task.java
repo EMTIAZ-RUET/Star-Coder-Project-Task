267eb4ecc1016959fbf48b9d78cb9ca17af9753a
EMTIAZ UDDIN AHMED <56144461+EMTIAZ-RUET@users.noreply.github.com>
2024-02-01 22:19:42 +0600

    Add the task

---
+++ task-management/src/main/java/com/emtiaz/task/management/entities/task.java
@@ -0,0 +1,51 @@
+package com.emtiaz.task.management.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="tasks")
+
+public class task {
+
+    private Long Id;
+    private String name;
+    private String type;
+
+    private String descripton;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescripton() {
+        return descripton;
+    }
+
+    public void setDescripton(String descripton) {
+        this.descripton = descripton;
+    }
+}
