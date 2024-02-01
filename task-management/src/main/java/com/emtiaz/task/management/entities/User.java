0bb497d3940abb4fc7cf6a788abda1fe28947701
EMTIAZ UDDIN AHMED <56144461+EMTIAZ-RUET@users.noreply.github.com>
2024-02-01 08:58:32 +0600

    Update User.java

--- task-management/src/main/java/com/emtiaz/task/management/entities/User.java
+++ task-management/src/main/java/com/emtiaz/task/management/entities/User.java
@@ -22,7 +22,41 @@ public class User {
 
     private String password;
 
-    private UserRole userrole;
+    private UserRole userRole;
 
+    public Long getId() {
+        return Id;
+    }
 
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
