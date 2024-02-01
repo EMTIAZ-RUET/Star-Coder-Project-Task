eca71c11ea3c641a0e45432f0c26cdcf506ad924
EMTIAZ UDDIN AHMED <56144461+EMTIAZ-RUET@users.noreply.github.com>
2024-02-01 22:25:37 +0600

    Add files via upload

---
+++ task-management/src/main/java/com/emtiaz/task/management/dtos/TaskDto.java
@@ -0,0 +1,46 @@
+package com.emtiaz.task.management.dtos;
+
+import lombok.Data;
+
+@Data
+
+public class TaskDto {
+
+    private Long Id;
+    private String name;
+    private String type;
+
+    private String descripton;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescripton() {
+        return descripton;
+    }
+
+    public void setDescripton(String descripton) {
+        this.descripton = descripton;
+    }
+}
