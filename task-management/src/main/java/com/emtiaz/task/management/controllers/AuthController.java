d558e61b18ae3e13cda581e617f04f0fea7c3c3e
EMTIAZ UDDIN AHMED <56144461+EMTIAZ-RUET@users.noreply.github.com>
2024-02-01 15:03:48 +0600

    Update AuthController.java

--- task-management/src/main/java/com/emtiaz/task/management/controllers/AuthController.java
+++ task-management/src/main/java/com/emtiaz/task/management/controllers/AuthController.java
@@ -1,16 +1,41 @@
 package com.emtiaz.task.management.controllers;
 
 
+import com.emtiaz.task.management.dtos.AuthenticationRequest;
+import com.emtiaz.task.management.dtos.AuthenticationResponse;
 import com.emtiaz.task.management.dtos.SignupRequest;
 import com.emtiaz.task.management.dtos.UserDto;
+import com.emtiaz.task.management.entities.User;
+import com.emtiaz.task.management.repositories.UserRepository;
 import com.emtiaz.task.management.services.AuthService;
+import com.emtiaz.task.management.services.jwt.UserService;
+import com.emtiaz.task.management.utilis.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +46,13 @@ public class AuthController {
     private final AuthService authService;
 
 
+    private final AuthenticationManager authenticationManager;
+
+    private final UserService userService;
+
+    private final JwtUtil jwtUtil;
+
+    private final UserRepository userRepository;
 
 
     @PostMapping("/register")
@@ -35,5 +67,38 @@ public class AuthController {
               return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad Request !");
 
           return  ResponseEntity.status(HttpStatus.CREATED).body(createdUserDto);
+    }
+
+    @PostMapping("/login")
+
+    public AuthenticationResponse createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws BadCredentialsException, DisabledException, UsernameNotFoundException
+    {
+        try
+        {
+             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(),authenticationRequest.getPassword()));
+
+
+        }
+        catch(BadCredentialsException e)
+        {
+            throw new BadCredentialsException("Incorrect username or password");
+        }
+
+        final UserDetails userDetails = userService.userDetailsService().loadUserByUsername(authenticationRequest.getEmail());
+
+        Optional<User> optionalUser = userRepository.findFirstByEmail(userDetails.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        AuthenticationResponse authenticationResponse = new AuthenticationResponse();
+
+        if(optionalUser.isPresent())
+        {
+            authenticationResponse.setJwt(jwt);
+            authenticationResponse.setUserId(optionalUser.get().getId());
+            authenticationResponse.setUserRole(optionalUser.get().getUserRole());
+        }
+
+        return authenticationResponse;
     }
 }
