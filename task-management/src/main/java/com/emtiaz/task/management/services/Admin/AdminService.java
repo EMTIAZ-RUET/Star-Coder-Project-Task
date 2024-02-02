3edf0492b2256ccb149b295f16ea219c98b34df9
EMTIAZ UDDIN AHMED <56144461+EMTIAZ-RUET@users.noreply.github.com>
2024-02-02 19:40:03 +0600

    Update AdminService.java

--- task-management/src/main/java/com/emtiaz/task/management/services/Admin/AdminService.java
+++ task-management/src/main/java/com/emtiaz/task/management/services/Admin/AdminService.java
@@ -2,7 +2,14 @@ package com.emtiaz.task.management.services.Admin;
 
 import com.emtiaz.task.management.dtos.TaskDto;
 
+import java.util.List;
+
 public interface AdminService {
 
     boolean postTask(TaskDto taskDto);
+
+    List<TaskDto> getAllTask();
+
+    void deleteCar(Long carId);
+
 }
