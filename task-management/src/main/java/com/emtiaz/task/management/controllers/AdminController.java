15d7d84986a16f60b13510d75982ea9023c704cd
EMTIAZ UDDIN AHMED <56144461+EMTIAZ-RUET@users.noreply.github.com>
2024-02-02 19:19:02 +0600

    Update AdminController.java

--- task-management/src/main/java/com/emtiaz/task/management/controllers/AdminController.java
+++ task-management/src/main/java/com/emtiaz/task/management/controllers/AdminController.java
@@ -6,10 +6,9 @@ import com.emtiaz.task.management.services.Admin.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,12 +21,21 @@ public class AdminController {
 
     public ResponseEntity<?> postTask(@ModelAttribute TaskDto taskDto)
     {
-       boolean success =  adminService.postTask(taskDto);
+        boolean success =  adminService.postTask(taskDto);
 
-       if(success)
-           return ResponseEntity.status(HttpStatus.CREATED).build();
+        if(success)
+            return ResponseEntity.status(HttpStatus.CREATED).build();
 
-       return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @GetMapping("/tasks")
+
+    public  ResponseEntity<List<TaskDto>>getAllTask()
+    {
+         List<TaskDto>taskDtoList = adminService.getAllTask();
+
+         return ResponseEntity.ok(taskDtoList);
     }
 
 }
